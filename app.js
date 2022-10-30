708be3509a97a420f76bdad18863efdafe21f99d
Dimitris <jvtiko@yahoo.gr>
2022-10-30 20:25:39 +0100

    1st commit

---
+++ app.js
@@ -0,0 +1,9 @@
+ export const getGreetingText = (hourOfDay) => {
+    if (hourOfDay < 12) {
+        return "Good morning!"
+    } else if (hourOfDay < 17) {
+        return "Good afternoon!"
+    } else {
+        return "Good evening!"
+    }
+};
